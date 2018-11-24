1adce0089233b47cfe2ff7cf3aa2e4e19b243477
Steve Achord <achord@gmail.com>
2018-11-24 10:21:59 -0600

    New package files

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,74 @@
+// Webpack 4
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+// TODO get minify css to work with node-sass
 module.exports = {
+  entry: { main: './src/app.js' },
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
         test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader"
-        ]
-      }
+        use: ExtractTextPlugin.extract(
+          {
+            fallback: 'style-loader',
+            use: ['css-loader', 'sass-loader'],
+          })
+      },
+      // {
+      //   test: /\.css$/,
+      //   use: ExtractTextPlugin.extract(
+      //     {
+      //       fallback: 'style-loader',
+      //       use: [{ 
+      //         loader: 'css-loader', 
+      //         options: { 
+      //           minimize: true 
+      //         } 
+      //       }]
+      //     })
+      // }
     ]
   },
   plugins: [
+    // new HtmlWebpackPlugin({
+    //   inject: false,
+    //   hash: true,
+    //   template: './src/index.html',
+    //   filename: 'index.html'
+    // }),
     new BrowserSyncPlugin({
       // browse to http://localhost:3000/ during development,
       // ./public directory is being served
       host: 'localhost',
       port: 3000,
       server: { baseDir: ['./'] },
-      files:['./*.html'],
+      // files:[
+      //   './*.html',
+      //   './**/*.scss'
+      // ],
       injectCss:true
     }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
+    new ExtractTextPlugin('main.css'),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.optimize\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default', { discardComments: { removeAll: true } }],
+      },
+      canPrint: true
+    }),
+    new OptimizeCssAssetsPlugin({cssProcessorOptions: {map: {inline: false, annotations: true}}})
   ]
 };
